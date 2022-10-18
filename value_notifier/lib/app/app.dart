4de12923e2a0440fab77216e6a70fcfe15cd6764
Rodrigo Dittrich <rodrigo@microsys.inf.br>
2022-10-18 06:06:31 -0300

    #Com setState

---
+++ value_notifier/lib/app/app.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+import 'package:value_notifier/app/home_page.dart';
+
+class App extends StatelessWidget {
+  const App({
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      theme: ThemeData(
+        primaryColor: Colors.blueAccent,
+        useMaterial3: true,
+      ),
+      debugShowCheckedModeBanner: false,
+      home: const HomePage()
+    );
+  }
+}
